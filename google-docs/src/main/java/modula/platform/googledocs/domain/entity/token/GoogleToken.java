017c5b36079b7761076b9bf41974d000d55be792
Ivan Varyuhin <varyuhin03@mail.ru>
2025-03-21 18:51:47 +0300

    feat(auth): authentication with code, store tokens in postgres

--- google-docs/src/main/java/modula/platform/googledocs/domain/entity/token/GoogleToken.java
+++ google-docs/src/main/java/modula/platform/googledocs/domain/entity/token/GoogleToken.java
@@ -1,30 +1,31 @@
-//package modula.platform.googledocs.domain.entity.token;
-//
-//import jakarta.persistence.*;
-//import lombok.Builder;
-//import lombok.Data;
-//import lombok.Getter;
-//import lombok.Setter;
-//
-//@Entity
-//@Data
-//public class GoogleToken {
-//
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    private Long id;
-//
-//    private String userEmail;
-//
+package modula.platform.googledocs.domain.entity.token;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Data
+@Table(name = "google_tokens")
+public class GoogleToken {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String userEmail;
+
+    @Column(length = 2048)
+    private String accessToken;
+
+    @Column(length = 2048)
+    private String idToken;
+
+    @Column(length = 2048)
+    private String refreshToken;
+
 //    @Column(length = 2048)
-//    private String accessToken;
-//
-//    @Column(length = 2048)
-//    private String idToken;
-//
-//    @Column(length = 2048)
-//    private String refreshToken;
-//
-//    @Column(length = 2048)
-//    private String scopes;
-//}
+    private String scopes;
+}
