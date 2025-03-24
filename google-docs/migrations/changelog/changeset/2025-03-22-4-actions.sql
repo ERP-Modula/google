33b1dbba7d55b4628ed54180237f98a22af16825
Ivan Varyuhin <varyuhin03@mail.ru>
2025-03-24 18:17:45 +0300

    ref(api): working migrations

---
+++ google-docs/migrations/changelog/changeset/2025-03-22-4-actions.sql
@@ -0,0 +1,1 @@
+INSERT INTO google_docs.actions (id, category, description, label, name) VALUES (1, 'Document', 'Retrieve a list of documents', 'List Documents', 'listFiles');
---
+++ google-docs/migrations/changelog/changeset/2025-03-22-5-nested_field_options.sql
@@ -0,0 +1,1 @@
+INSERT INTO google_docs.nested_field_options (id, is_default_value, label, type, value) VALUES (1, true, 'My Drive', 'string', 'drive');
